e7b3ecceb2aec2655d13ef66f54652ffc3be5e8e
Gino Villalpando <grieveous@live.com>
2019-10-23 14:21:46 -0600

    added wireframes for capstone project and created this repository for a copy of my capstone code

---
+++ wireframes/wireframes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title> The WireFrames for Capstone The Deep Dive Dawgs </title>
+	</head>
+		<body>
+		 <div>
+			 <img src="wireframe-capstone-mobile.png" alt="Mobile Wireframe" />
+			 <img src="fullscreen-wireframe.png" alt="Fullscreen Wireframe" />
+		 </div>
+		</body>
+</html>
+
