cc2102cf3026d7a9ad4d35b4f762b11aed6c272a
Gino Villalpando <grieveous@live.com>
2019-10-25 00:36:15 -0600

    wrote user class accessors, mutators, getters, setters and pdo methods

--- wireframes/wireframes.php
+++ wireframes/wireframes.php
@@ -1,14 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title> The WireFrames for Capstone The Deep Dive Dawgs </title>
-	</head>
-		<body>
-		 <div>
-			 <img src="wireframe-capstone-mobile.png" alt="Mobile Wireframe" />
-			 <img src="fullscreen-wireframe.png" alt="Fullscreen Wireframe" />
-		 </div>
-		</body>
+<head>
+	<meta charset="UTF-8">
+	<title> The WireFrames for Capstone The Deep Dive Dawgs </title>
+	<link rel="stylesheet" type="text/css" href="stylesheetWireframes.css">
+</head>
+	<body>
+	<!-- header for wireframes -->
+	<div class="headerFont">
+		<h1>Wireframes</h1>
+	</div>
+	<!-- Image sources of wireframes
+		  wireframe-capstone-mobile.png is our capstone mobile view of the site
+		  Fullscreen-wireframe.png is our Fullscreen view for our site
+		  added additional page wireframes-->
+		<div>
+			<img src= "wireframe-images/wireframe-capstone-mobile.png" alt="Mobile index Wireframe">
+			<img src= "wireframe-images/fullscreen-wireframe.png" alt="Fullscreen index Wireframe">
+			<img src="wireframe-images/search-results.png" alt="Search Results' page Wireframe">
+			<img src="wireframe-images/about-us-wireframe.png" alt="About Us page Wireframe">
+			<img src="wireframe-images/faq-wireframe.png" alt="FAQ page Wireframe">
+			<img id="mobileImages" src="wireframe-images/search-results-mobile-wireframe.jpg" alt="Search Results' mobile page Wireframe"/>
+			<img id="mobileImages" src="wireframe-images/about-us-mobile-wireframe.jpg" alt="Mobile About Us Page Wireframe">
+			<img id="mobileImages" src="wireframe-images/faq-mobile-wireframe.jpg" alt="FAQ mobile page wireframes">
+		</div>
+	</body>
 </html>
 
